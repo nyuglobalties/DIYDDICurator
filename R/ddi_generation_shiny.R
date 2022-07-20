23566152ad19e637161a72ac2574ac0fdb0ca3cd
dswoulf <78369907+dswoulf@users.noreply.github.com>
2022-07-20 10:35:25 -0400

    feat: add DDI codebook download button

--- R/ddi_generation_shiny.R
+++ R/ddi_generation_shiny.R
@@ -1,15 +1,24 @@
 ddi_generation_ui <- function(id) {
   ns <- NS(id)
   tabPanel("DDI Codebook",
-           htmltidy::xmlviewOutput(ns("XML"))
+           htmltidy::xmlviewOutput(ns("XML")),
+           downloadButton(ns("download_DDI"), "Download codebook")
   )
 }
 
-ddi_generation_server <- function(id, dat) {
+ddi_generation_server <- function(id, dat, filepth) {
   moduleServer(id, function(input, output, session) {
     output$XML <- htmltidy::renderXmlview({
       req(dat())
       htmltidy::xml_view(generate_ddi_codebook(dat()))
     })
+    
+    output$download_DDI <- downloadHandler(
+      filename = "codebook.xml",
+      content = function(file) {
+        xml2::write_xml(generate_ddi_codebook(dat()), file)
+      },
+      contentType = "text/xml"
+    )
   })
 }
